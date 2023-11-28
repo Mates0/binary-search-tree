b6bb7cbcbd279711edb1c197a37edcea4ca29117
Mates0 <matyas.vanko@educanet.cz>
2023-11-28 11:40:27 +0100

    bla

--- src/main/java/cz/educanet/Main.java
+++ src/main/java/cz/educanet/Main.java
@@ -1,7 +1,17 @@
 package cz.educanet;
 
 public class Main {
+    private static final int root = 7;
     public static void main(String[] args) {
 
+        Bst<Integer> bst = new Bst<>(root);
+
+        for (int i = 0; i < root; i++)
+            bst.insert(i);
+
+        System.out.println(bst.contains(2));
+        System.out.println(bst.contains(3));
+        System.out.println(bst.contains(69));
+        System.out.println(bst.contains(420));
     }
 }
